cab1163ed4bdf6dbbcb4b0c4ce1062af841fb40f
Radoslaw Cieciwa <radoslaw.cieciwa@corp.badoo.com>
2017-10-18 23:22:33 +0100

    Install updated.

--- install.sh
+++ install.sh
@@ -6,13 +6,21 @@
 # 
 
 THISSCRIPT=`basename $0`
+VERBOSE=true
+TOOL_BIN_PATH="/usr/local/bin"
 
 _install() {
-	ln -s -f $1 "/usr/local/bin/$2"
+	ln -s -f $1 "$TOOL_BIN_PATH/$2"
+	if [ $VERBOSE == true ]; then
+		echo "Linked: $1 ==> $TOOL_BIN_PATH/$2"
+	fi
 }
 
 _uninstall() {
-	rm "/usr/local/bin/$1"
+	rm "$TOOL_BIN_PATH/$1"
+	if [ $VERBOSE == true ]; then
+		echo "Removed: $1"
+	fi
 }
 
 _print_help() {
@@ -28,14 +36,33 @@ Examples:
 EOF
 }
 
+# to define new tool to install, please add
+# new entry in array following format:
+# "tool-name:tool-path"
+# tool-name - your tool name, can be anything
+# tool-path - absolute path to tooling that will be linked in /usr/local/bin
+TOOLS_DEFINITION=( "qsync:`pwd`/qsync.sh" "git_move:`pwd`/git_move_tool.sh" "prepare_for_git_move:`pwd`/prepare_for_git_move.sh")
+
 if [ "$1" = "--help" -o "$1" = "-help" -o "$1" = "-h" ]; then
 	_print_help
 else 
 	if [ "$1" = "-r" ]; then
-		_uninstall qsync
+		for entry in "${TOOLS_DEFINITION[@]}"; do
+    		BIN="${entry%%:*}"
+    		_uninstall $BIN
+		done
 	elif [ "$1" = "-l" ]; then
 		ls -l /usr/local/bin/ | grep "\->"
 	else
-		_install "`pwd`/qsync.sh" qsync
+		for entry in "${TOOLS_DEFINITION[@]}"
+		do
+    		BIN="${entry%%:*}"
+    		DIR="${entry#*:}"
+    		_install $DIR $BIN 
+		done
+		if [ $VERBOSE == true ]; then
+			echo "=== CAUTION ==="
+			echo "To uninstall use -r option"
+		fi
 	fi
 fi 
